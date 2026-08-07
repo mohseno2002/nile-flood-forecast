dd57a916746c6ada8f2751c38aa8e0a38fe77031
mohseno2002 <mohseno2002@gmail.com>
2026-08-07 10:38:19 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* مِرقاب الفيضان — Service Worker */
-var VERSION = "nile-flood-forecast-v2.56";
+var VERSION = "nile-flood-forecast-v2.57";
 var SHELL = ["./", "./index.html", "./manifest.json",
              "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
 /* استثناء إلزامى: نطاقات البيانات الحية لا تدخل الكاش إطلاقاً (no-store)
